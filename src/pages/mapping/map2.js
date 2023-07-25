2e298e2ca59e38a846f3e1bfa0e58d3569cb4f0e
Kelvin Njuguna <ozzaynetwork@gmail.com>
2023-07-25 15:39:51 +0300

    kelvin: the map is better now

--- src/pages/mapping/map2.js
+++ src/pages/mapping/map2.js
@@ -27,14 +27,9 @@ import InActiveIcon from "./mappingAssets/images/inactive-agent.svg";
 
 import $ from 'jquery';
 
-// import   JquerryAccordion   from "./customPlugins/jquerryAccordion";
-const MapView =()=> {
-    const [loading, setLoading] = useState(false);
-    const [selectedMarker, setSelectedMarker] = useState(null);
-    
-    
-   
-    const [ selected, setSelected ] = useState({});
+
+const InitMap = React.memo(({ locations }) => {
+
     const [mapZoom,setMapZoom]=useState(10)
     const [mapCenter,setMapCenter]=useState({ lat: -0.5306294206232787, lng: 34.46123164921829 })
     const [inactiveAgents,setInactiveAgents]=useState([
@@ -75,6 +70,101 @@ const MapView =()=> {
         }
       ])
 
+    const center = useMemo(
+      () => (mapCenter),
+      [],
+    )
+    const options = {
+      styles: mapStyles,
+      disableDefaultUI: true,
+      zoomControl: true,
+      scrollwheel: true,
+      mapTypeControl: false,
+      fullscreenControl: true,
+      streetViewControl: true,
+    }
+  
+    const [selected, setSelected] = useState({})
+  
+    // Memoize the onSelect function using useCallback
+    const onSelect = useCallback((item) => {
+      setSelected(item)
+    }, [])
+  
+    return(
+        <GoogleMap
+            center={mapCenter}
+            zoom={mapZoom}
+            mapContainerClassName='h-100'
+            options={options}
+        >
+            {
+                inactiveAgents?.map(item => {
+                return (
+                <MarkerF
+                    key={item?.name} 
+                    position={item?.location}
+                    onClick={() => onSelect(item)}
+                    icon={{
+                        url: InActiveIcon,
+                        scaledSize: new window.google.maps.Size(60, 60)
+                    }}
+                />
+                )
+                })
+            }
+
+            {
+                selected.location && 
+                (
+                <InfoWindow
+                    position={selected?.location}
+                    clickable={true}
+                    options={{
+                        pixelOffset: new window.google.maps.Size(0, -65)
+                        }}
+                    
+                    onCloseClick={() => setSelected({})}
+                    >
+                    <>
+                        <p class="d-none">agent|agent num</p>
+                        <h6 class="text-capitalize d-flex align-items-center">
+                            <span class="offline-agent mr-2 me-2"></span> 
+                            <span>{selected?.name}<small><strong>(INACTIVE)</strong></small></span>
+                        </h6>
+                        <p class="pb-0 mb-0">Last seen at <strong>Tom Mboya Street</strong> at <strong>2:06 PM</strong></p>
+                        <p>Most recent activity: <strong>Queried Car plate Number KBW 2589T at 11:41PM</strong></p>
+
+                        <div>
+                            <div class="listview__header text-align-left text-capitalize text-left mb-2">
+                                Collected <strong>KES 25,000</strong> towards the <strong>KES 450,000</strong> Target.
+                            </div>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-success"  style={{width:'25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+                    </>
+
+                </InfoWindow>
+                )
+            }
+
+        </GoogleMap>
+
+    ) 
+  })
+
+const MapView =()=> {
+    const [loading, setLoading] = useState(false);
+    const [selectedMarker, setSelectedMarker] = useState(null);
+    
+    
+   
+    const [ selected, setSelected ] = useState({});
+    
+   
+
     const [mapContainer,setMapContainer]=useState(null)
 
     //map styles
@@ -88,12 +178,12 @@ const MapView =()=> {
         streetViewControl: true,
       }
 
-    const [mapRenderOptions,setMapRenderOptions]=useState({
-        zoom:mapZoom,
-        center:{mapCenter},
-        options:{options},
-        mapContainerClassName:'h-100'       
-    })
+    // const [mapRenderOptions,setMapRenderOptions]=useState({
+    //     zoom:mapZoom,
+    //     center:{mapCenter},
+    //     options:{options},
+    //     mapContainerClassName:'h-100'       
+    // })
       
     //   clicked marker
     const onSelect = item => {
@@ -159,82 +249,6 @@ const MapView =()=> {
     });
 
    
-
-   
-
-    function InitMap(){
-            
-        return(
-            <GoogleMap
-                center={mapCenter}
-                zoom={mapZoom}
-                mapContainerClassName='h-100'
-                options={options}
-            >
-                {
-                    inactiveAgents?.map(item => {
-                    return (
-                    <MarkerF
-                        key={item?.name} 
-                        position={item?.location}
-                        onClick={() => onSelect(item)}
-                        icon={{
-                            url: InActiveIcon,
-                            scaledSize: new window.google.maps.Size(60, 60)
-                        }}
-                    />
-                    )
-                    })
-                }
-
-                {
-                    selected.location && 
-                    (
-                    <InfoWindow
-                        position={selected?.location}
-                        clickable={true}
-                        options={{
-                            pixelOffset: new window.google.maps.Size(0, -65)
-                            }}
-                        
-                        onCloseClick={() => setSelected({})}
-                        >
-                        <>
-                            <p class="d-none">agent|agent num</p>
-                            <h6 class="text-capitalize d-flex align-items-center">
-                                <span class="offline-agent mr-2 me-2"></span> 
-                                <span>{selected?.name}<small><strong>(INACTIVE)</strong></small></span>
-                            </h6>
-                            <p class="pb-0 mb-0">Last seen at <strong>Tom Mboya Street</strong> at <strong>2:06 PM</strong></p>
-                            <p>Most recent activity: <strong>Queried Car plate Number KBW 2589T at 11:41PM</strong></p>
-
-                            <div>
-                                <div class="listview__header text-align-left text-capitalize text-left mb-2">
-                                    Collected <strong>KES 25,000</strong> towards the <strong>KES 450,000</strong> Target.
-                                </div>
-                                <div class="progress">
-                                    <div class="progress-bar progress-bar-success"  style={{width:'25%'}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </div>
-
-                        </>
-
-                    </InfoWindow>
-                    )
-                }
-
-            </GoogleMap>
-
-        )       
-     }
-
-     const MemodMap = React.memo(InitMap)
-
-
-   
-   
-
-   
    if(!isLoaded) return(<>
     <div className="content-loader-container bg-black bg-opacity-50">
                 <div className="bg-white p-3 ">
@@ -262,7 +276,7 @@ const MapView =()=> {
         </Helmet>   
         <div className="container-fluid">
             <div className="row d-sm-none d-md-flex">
-                <div className="col-12">
+                <div className="col-12 d-none">
                     <div className="page-title-box d-sm-flex align-items-center justify-content-between">
                         <h4 className="mb-sm-0 font-size-18">Map View 2</h4>
 
@@ -296,140 +310,8 @@ const MapView =()=> {
                         </div>
                         <div class="card-body">
                             <div id="map" class="w-100 mt-1 h-100 live-map-cont main-map-container">
-                                <MemodMap></MemodMap>
+                                <InitMap></InitMap>
                             </div>
-                        </div>
-
-                    </div>
-
-                    <div class="">
-                        <div class="position-relative ">
-
-                            <div class="form-group map-searcher">
-
-                                <input type="text" class="form-control form-control-lg text-black"
-                                    placeholder="Search for a location" id="pac-input"/>
-                                <button title="Clear search results" class="btn btn-transparent border-0 clear-map"><i
-                                        class="mdi mdi-close d-none"></i></button>
-                            </div>
-
-                            <div class="map-key-card card">
-                                <div class="card-body">
-                                    <i class="mdi mdi-key-variant"></i>
-                                </div>
-                            </div>
-
-                            <div class="card map-card animated slideInLeft d-none">
-                                <span class="close-map-key"><i class="zmdi zmdi-close"></i></span>
-                                <div class="card-body">
-                                    <h4 class="card-title">Map Key</h4>
-
-                                </div>
-                                <div class="listview py-4 map-key">
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/damaged.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Damaged Ad Plates</strong></div>
-                                        </div>
-                                    </span>
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/available.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Available Ad Plates</strong></div>
-                                        </div>
-                                    </span>
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/rented.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Rented Ad Plates</strong></div>
-                                        </div>
-                                    </span>
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/multiple-plates.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Multiple sided Ad Plates</strong></div>
-                                        </div>
-                                    </span>
-
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/rejected.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Rejected Applications</strong></div>
-                                        </div>
-                                    </span>
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/pending.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Pending applications</strong></div>
-                                        </div>
-                                    </span>
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/approved.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Approved Applications</strong></div>
-                                        </div>
-                                    </span>
-
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/warning.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Incidents</strong></div>
-                                        </div>
-                                    </span>
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/active-agent.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>active-agent</strong></div>
-                                        </div>
-                                    </span>
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/inactive-agent.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Inactive agents</strong></div>
-                                        </div>
-                                    </span>
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/parliament.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>City Hall</strong></div>
-                                        </div>
-                                    </span>
-
-
-                                    <span class="listview__item py-0">
-                                        <img src="assets/images/map-assets/flag.svg" class="listview__img" alt=""/>
-
-                                        <div class="listview__content d-flex align-items-center">
-                                            <div class="listview__heading"><strong>Service/Collection Point</strong></div>
-                                        </div>
-                                    </span>
-
-
-
-                                </div>
-                            </div>
-
                         </div>
 
                     </div>
